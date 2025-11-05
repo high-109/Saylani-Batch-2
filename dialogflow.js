1a93906d1d73599c0a115697c00b07ca89110981
Saylani-GenAIChatbot <highrated109@gmail.com>
2025-11-05 10:11:17 +0500

    Update dialogflow.js

--- dialogflow.js
+++ dialogflow.js
@@ -9,6 +9,9 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 
+app.get('/', (req, res) => {
+    res.send("Server is running!");
+});
 app.post("/webhook", async (req, res) => {
     var id = (res.req.body.session).substr(43);
     console.log(id)
@@ -16,19 +19,31 @@ app.post("/webhook", async (req, res) => {
 
     function hi(agent) {
         console.log(`intent  =>  hi`);
-        agent.add("")
+        agent.add("Hello there, I am hammad from serever side!")
     }
 
-    function sendNotes(agent) {
+    function fallback(agent) {
         const { number , date , email} = agent.parameters;
-       agent.add("")
+       agent.add("Fallback Intent called!")
+    }
+
+       function booking(agent) {
+       const { arrival , destination , date, email, phone} = agent.parameters;
+       agent.add(`Hi there, Your booking from ${arrival} to ${destination} at ${date} date has been booked! we sent an email at ${email} and also in your phone number ${phone} from server.!`)
+       console.log(arrival)
+       console.log(destination)
+       console.log(date)
+       console.log(phone)
+       console.log(email)
     }
 
     let intentMap = new Map();
-    intentMap.set('hi', hi); 
-    intentMap.set('sendNotes', sendNotes);
+    intentMap.set('Default Welcome Intent', hi); 
+    intentMap.set('Default Fallback Intent', fallback);
+    intentMap.set('flight-booking', booking);
     agent.handleRequest(intentMap);
 })
+
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 });
